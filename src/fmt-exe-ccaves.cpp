69be0573a8e1df596a2311f685dca6ee6b0de1b4
Adam Nielsen <malvineous@shikadi.net>
2011-05-22 13:31:26 +1000

    Tidy up Crystal Caves .exe file offsets and split map into intro/finale/map

--- src/fmt-exe-ccaves.cpp
+++ src/fmt-exe-ccaves.cpp
@@ -23,27 +23,74 @@
 namespace camoto {
 namespace gamearchive {
 
-#define MAPSIZE   (41*36)
-#define LEVELSIZE (41*24)
-#define SMLEVEL   (41*23)
+/// Offset of the first byte of map data
+#define MAPDATA_START 0x8CE0
+
+// Map sizes
+#define SZ_INT    (41*5)
+#define SZ_FIN    (41*6)
+#define SZ_MAP    (41*25)
+#define SZ_NORMAL (41*24)
+#define SZ_SMALL  (41*23)
+
+#define SZ_L01  SZ_NORMAL
+#define SZ_L02  SZ_NORMAL
+#define SZ_L03  SZ_NORMAL
+#define SZ_L04  SZ_NORMAL
+#define SZ_L05  SZ_NORMAL
+#define SZ_L06  SZ_NORMAL
+#define SZ_L07  SZ_SMALL
+#define SZ_L08  SZ_SMALL
+#define SZ_L09  SZ_NORMAL
+#define SZ_L10  SZ_NORMAL
+#define SZ_L11  SZ_NORMAL
+#define SZ_L12  SZ_NORMAL
+#define SZ_L13  SZ_NORMAL
+#define SZ_L14  SZ_SMALL
+#define SZ_L15  SZ_NORMAL
+#define SZ_L16  SZ_NORMAL
+
+// Map offsets
+#define OFF_INT  MAPDATA_START
+#define OFF_FIN  OFF_INT + SZ_INT
+#define OFF_MAP  OFF_FIN + SZ_FIN
+#define OFF_L01  OFF_MAP + SZ_MAP
+#define OFF_L02  OFF_L01 + SZ_L01
+#define OFF_L03  OFF_L02 + SZ_L02
+#define OFF_L04  OFF_L03 + SZ_L03
+#define OFF_L05  OFF_L04 + SZ_L04
+#define OFF_L06  OFF_L05 + SZ_L05
+#define OFF_L07  OFF_L06 + SZ_L06
+#define OFF_L08  OFF_L07 + SZ_L07
+#define OFF_L09  OFF_L08 + SZ_L08
+#define OFF_L10  OFF_L09 + SZ_L09
+#define OFF_L11  OFF_L10 + SZ_L10
+#define OFF_L12  OFF_L11 + SZ_L11
+#define OFF_L13  OFF_L12 + SZ_L12
+#define OFF_L14  OFF_L13 + SZ_L13
+#define OFF_L15  OFF_L14 + SZ_L14
+#define OFF_L16  OFF_L15 + SZ_L15
+
 FixedArchiveFile ccaves_file_list[] = {
-	{0x8CE0,                            MAPSIZE,   "e1map.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 0,  LEVELSIZE, "e1l01.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 1,  LEVELSIZE, "e1l02.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 2,  LEVELSIZE, "e1l03.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 3,  LEVELSIZE, "e1l04.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 4,  LEVELSIZE, "e1l05.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 5,  LEVELSIZE, "e1l06.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 6,  SMLEVEL,   "e1l07.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 6 + SMLEVEL, SMLEVEL, "e1l08.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 6 + SMLEVEL*2, LEVELSIZE, "e1l09.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 7 + SMLEVEL*2, LEVELSIZE, "e1l10.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 8 + SMLEVEL*2, LEVELSIZE, "e1l11.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 9 + SMLEVEL*2, LEVELSIZE, "e1l12.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 10 + SMLEVEL*2, LEVELSIZE, "e1l13.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 11 + SMLEVEL*2, SMLEVEL, "e1l14.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 11 + SMLEVEL*3, LEVELSIZE, "e1l15.ccl", FILTER_NONE},
-	{0x8CE0 + MAPSIZE + LEVELSIZE * 12 + SMLEVEL*3, LEVELSIZE, "e1l16.ccl", FILTER_NONE},
+	{OFF_INT, SZ_INT, "e1int.ccl", FILTER_NONE},
+	{OFF_FIN, SZ_FIN, "e1fin.ccl", FILTER_NONE},
+	{OFF_MAP, SZ_MAP, "e1map.ccl", FILTER_NONE},
+	{OFF_L01, SZ_L01, "e1l01.ccl", FILTER_NONE},
+	{OFF_L02, SZ_L02, "e1l02.ccl", FILTER_NONE},
+	{OFF_L03, SZ_L03, "e1l03.ccl", FILTER_NONE},
+	{OFF_L04, SZ_L04, "e1l04.ccl", FILTER_NONE},
+	{OFF_L05, SZ_L05, "e1l05.ccl", FILTER_NONE},
+	{OFF_L06, SZ_L06, "e1l06.ccl", FILTER_NONE},
+	{OFF_L07, SZ_L07, "e1l07.ccl", FILTER_NONE},
+	{OFF_L08, SZ_L08, "e1l08.ccl", FILTER_NONE},
+	{OFF_L09, SZ_L09, "e1l09.ccl", FILTER_NONE},
+	{OFF_L10, SZ_L10, "e1l10.ccl", FILTER_NONE},
+	{OFF_L11, SZ_L11, "e1l11.ccl", FILTER_NONE},
+	{OFF_L12, SZ_L12, "e1l12.ccl", FILTER_NONE},
+	{OFF_L13, SZ_L13, "e1l13.ccl", FILTER_NONE},
+	{OFF_L14, SZ_L14, "e1l14.ccl", FILTER_NONE},
+	{OFF_L15, SZ_L15, "e1l15.ccl", FILTER_NONE},
+	{OFF_L16, SZ_L16, "e1l16.ccl", FILTER_NONE},
 };
 
 EXE_CCavesType::EXE_CCavesType()
