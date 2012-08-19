b280867895526b1617b237744dc2ba7e04b3d7d2
Adam Nielsen <malvineous@shikadi.net>
2012-08-19 12:41:20 +1000

    Only call GNU library functions when GNU libstdc++ is in use

--- examples/gamearch.cpp
+++ examples/gamearch.cpp
@@ -386,8 +386,10 @@ void extractAll(ga::ArchivePtr pArchive, bool bScript)
 
 int main(int iArgC, char *cArgV[])
 {
+#ifdef __GLIBCXX__
 	// Set a better exception handler
-	std::set_terminate( __gnu_cxx::__verbose_terminate_handler );
+	std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
+#endif
 
 	// Disable stdin/printf/etc. sync for a speed boost
 	std::ios_base::sync_with_stdio(false);
--- examples/gamecomp.cpp
+++ examples/gamecomp.cpp
@@ -41,8 +41,10 @@ namespace stream = camoto::stream;
 
 int main(int iArgC, char *cArgV[])
 {
+#ifdef __GLIBCXX__
 	// Set a better exception handler
-	std::set_terminate( __gnu_cxx::__verbose_terminate_handler );
+	std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
+#endif
 
 	// Disable stdin/printf/etc. sync for a speed boost
 	std::ios_base::sync_with_stdio(false);
