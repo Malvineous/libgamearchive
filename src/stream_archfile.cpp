1b4bd21c7244ab3aade924056a121d902736feca
Adam Nielsen <malvineous@shikadi.net>
2015-02-18 11:26:56 +1000

    New archfile stream for providing direct stream::sub access to FATArchive files
    This new class inherits from stream::sub, but provides the offset and length
    directly from the FATEntry fields.  This allows all open streams in an archive
    to be moved around automatically as other files are added and removed.
    
    It also holds only one strong reference to the FATArchive instance, so that it
    can call resize() as needed.  This also means the archive can be closed but the
    file can still be used successfully.

---
+++ src/stream_archfile.cpp
@@ -0,0 +1,141 @@
+/**
+ * @file  stream_archfile.cpp
+ * @brief Provide a stream that accesses a file within an Archive instance.
+ *
+ * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <camoto/util.hpp>
+#include "stream_archfile.hpp"
+
+using namespace camoto;
+using namespace camoto::gamearchive;
+
+archfile_core::archfile_core(const Archive::FileHandle& id)
+	:	sub_core(0, 0),
+		id(const_cast<Archive::FileHandle&>(id)),
+		fat(dynamic_cast<const FATArchive::FATEntry *>(&*id))
+{
+}
+
+void archfile_core::relocate(stream::delta off)
+{
+	throw stream::error("archfile_core::relocate() should never be called");
+}
+
+void archfile_core::resize(stream::len len)
+{
+	throw stream::error("archfile_core::resize() should never be called");
+}
+
+stream::pos archfile_core::sub_start() const
+{
+	if (!this->fat->bValid) {
+		throw stream::error("Attempt to access closed or deleted file.");
+	}
+	return this->fat->iOffset + this->fat->lenHeader;
+}
+
+stream::len archfile_core::sub_size() const
+{
+	if (!this->fat->bValid) {
+		throw stream::error("Attempt to access closed or deleted file.");
+	}
+	return this->fat->storedSize;
+}
+
+
+input_archfile::input_archfile(const Archive::FileHandle& id,
+	std::shared_ptr<stream::input> content)
+	:	sub_core(0, 0), // length values are unused as we will be overriding them
+		input_sub(content, 0, 0),
+		archfile_core(id)
+{
+}
+
+
+output_archfile::output_archfile(std::shared_ptr<Archive> archive,
+	Archive::FileHandle id, std::shared_ptr<stream::output> content)
+	:	sub_core(0, 0), // length values are unused as we will be overriding them
+		output_sub(content, 0, 0, stream::fn_truncate_sub()),
+		archfile_core(id),
+		archive(archive)
+{
+}
+
+void output_archfile::truncate(stream::len size)
+{
+	if (this->sub_size() == size) return; // nothing to do
+	assert(this->id);
+
+	stream::len newRealSize;
+	if (this->id->fAttr & EA_COMPRESSED) {
+		// We're compressed, so the real and stored sizes are both valid
+		newRealSize = this->id->realSize;
+	} else {
+		// We're not compressed, so the real size won't be updated by a filter,
+		// so we need to update it here.
+		newRealSize = size;
+	}
+
+	// Resize the file in the archive.  This function will also tell the
+	// substream it can now write to a larger area.
+	// We are updating both the stored (in-archive) and the real (extracted)
+	// sizes, to handle the case where no filters are used and the sizes are
+	// the same.  When filters are in use, the flush() function that writes
+	// the filtered data out should call us first, then call the archive's
+	// resize() function with the correct real/extracted size.
+	this->archive->resize(this->id, size, newRealSize);
+
+	// After a truncate the file pointer is always left at the new EOF
+	try {
+		this->seekp(size, stream::start);
+	} catch (const stream::seek_error& e) {
+		throw stream::write_error("Archive resize function silently failed!  "
+			"Unable to seek to EOF after truncate: " + e.get_message());
+	}
+	return;
+}
+
+void output_archfile::setRealSize(stream::len newRealSize)
+{
+	this->archive->resize(this->id, this->id->storedSize, newRealSize);
+	return;
+}
+
+void output_archfile::flush()
+{
+	this->out_parent->flush();
+	if (this->archive.unique()) {
+		// We are the only user of the shared archive, so the caller has no other
+		// means to flush it.  So we will have to flush it for them.
+		this->archive->flush();
+	}
+	return;
+}
+
+
+archfile::archfile(std::shared_ptr<Archive> archive, Archive::FileHandle id,
+	std::shared_ptr<stream::inout> content)
+	:	sub_core(0, 0),
+		input_sub(content, 0, 0),
+		output_sub(content, 0, 0, stream::fn_truncate_sub()),
+		sub(content, 0, 0, stream::fn_truncate_sub()),
+		archfile_core(id),
+		input_archfile(id, content),
+		output_archfile(archive, id, content)
+{
+}
---
+++ src/stream_archfile.hpp
@@ -0,0 +1,201 @@
+/**
+ * @file  stream_archfile.hpp
+ * @brief Provide a stream that accesses a file within an Archive instance.
+ *
+ * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CAMOTO_STREAM_ARCHFILE_HPP_
+#define _CAMOTO_STREAM_ARCHFILE_HPP_
+
+#include <camoto/gamearchive/manager.hpp>
+#include <camoto/stream_sub.hpp>
+#include "fatarchive.hpp"
+
+namespace camoto {
+namespace gamearchive {
+
+/// Substream parts in common with read and write
+class archfile_core: virtual public stream::sub_core
+{
+	protected:
+		archfile_core(const Archive::FileHandle& id);
+
+	public:
+		/// Move the substream's start point within the parent stream.
+		/**
+		 * @param off
+		 *   Distance to move the stream.  Negative values move closer to
+		 *   the start of the parent stream.
+		 */
+		void relocate(stream::delta off);
+
+		/// Alter the size of the substream without affecting any data.
+		/**
+		 * This function should only be called by the creator of the stream if the
+		 * parent stream has been modified outside of the substream.
+		 *
+		 * Normally output_sub::truncate() would be used to correctly resize the
+		 * substream.  If in doubt, don't use this function!
+		 *
+		 * @param len
+		 *   New size of the substream.
+		 */
+		void resize(stream::len len);
+
+		/// Get the current offset into the parent stream.
+		/**
+		 * @return Current offset, relative to start of parent stream, where first
+		 *   byte in the substream sits.
+		 */
+		virtual stream::pos sub_start() const;
+
+		/// Get the current size of the window into the parent stream.
+		/**
+		 * @return Current size of substream, in bytes.  The last byte in the
+		 *   parent stream that can be read is at offset start() + size() - 1.
+		 */
+		virtual stream::len sub_size() const;
+
+		/// File handle for resizing/truncating.
+		Archive::FileHandle id;
+
+		/// FATEntry cast of id
+		/**
+		 * This avoids doing a dynamic_cast on each and every read/write.
+		 */
+		const FATArchive::FATEntry *fat;
+};
+
+/// Read-only stream to access a section within another stream.
+class input_archfile:
+	virtual public stream::input_sub,
+	virtual public archfile_core
+{
+	public:
+		/// Substream representing a file within an Archive.
+		/**
+		 * @param id
+		 *   FileHandle of file being opened.  Used to get current offset/size
+		 *   values for substream window.
+		 *
+		 * @param content
+		 *   Stream containing archive's raw content.  No filters should be applied,
+		 *   and this stream can be shared amongst other files, although at the time
+		 *   of writing this is not thread-safe.
+		 */
+		input_archfile(const Archive::FileHandle& id,
+			std::shared_ptr<stream::input> content);
+
+		using stream::input_sub::size;
+};
+
+/// Write-only stream to access a section within another stream.
+class output_archfile:
+	virtual public stream::output_sub,
+	virtual public archfile_core
+{
+	public:
+		/// Substream representing a file within an Archive.
+		/**
+		 * @param archive
+		 *   Archive containing the file to open.  Used to call resize() method
+		 *   if stream is ever resized.
+		 *
+		 * @param id
+		 *   FileHandle of file being opened.  Passed to Archive::resize() if
+		 *   stream is resized.  Also kept and used to get current offset/size
+		 *   values for substream window.
+		 *
+		 * @param content
+		 *   Stream containing archive's raw content.  No filters should be applied,
+		 *   and this stream can be shared amongst other files, although at the time
+		 *   of writing this is not thread-safe.
+		 */
+		output_archfile(std::shared_ptr<Archive> archive, Archive::FileHandle id,
+			std::shared_ptr<stream::output> content);
+
+		virtual void truncate(stream::len size);
+		virtual void flush();
+
+		/// Set the original (decompressed) size of this stream.
+		/**
+		 * This is just a convenience function to call Archive::resize().
+		 */
+		void setRealSize(stream::len newRealSize);
+
+	protected:
+		/// Archive handle for resizing/truncating.
+		std::shared_ptr<Archive> archive;
+};
+
+/// Read/write stream accessing a file within an Archive.
+class archfile:
+	virtual public stream::sub,
+	virtual public input_archfile,
+	virtual public output_archfile
+{
+	public:
+		/// Substream representing a file within an Archive.
+		/**
+		 * @copydetails output_archfile::output_archfile()
+		 */
+		archfile(std::shared_ptr<Archive> archive, Archive::FileHandle id,
+			std::shared_ptr<stream::inout> content);
+};
+
+template <class T>
+std::unique_ptr<T> applyFilter(std::unique_ptr<T> s, const std::string& filter)
+{
+	auto arch = dynamic_cast<archfile*>(s.get());
+	assert(arch);
+	if (filter.empty()) return s;
+
+	// The file needs to be filtered first
+	auto pFilterType = FilterManager::byCode(filter);
+	if (!pFilterType) {
+		throw stream::error(createString(
+			"could not find filter \"" << filter << "\""
+		));
+	}
+
+	return pFilterType->apply(
+		std::move(s),
+		[](stream::output_filtered* filt, stream::len newRealSize) {
+			archfile* arch;
+			while (filt) {
+				auto filt_content = filt->get_stream().get();
+				arch = dynamic_cast<archfile*>(filt_content);
+				if (arch) break;
+
+				// This isn't an archfile, so see if this is a layered filter and
+				// there's another one below it.
+				filt = dynamic_cast<stream::output_filtered*>(filt_content);
+			}
+			// If this fails, the underlying stream (even if it's buried under layers
+			// of output_filtered streams) isn't an archfile.
+			assert(arch);
+
+			if (arch) arch->setRealSize(newRealSize);
+			return;
+		}
+	);
+}
+
+} // namespace gamearchive
+} // namespace camoto
+
+#endif // _CAMOTO_STREAM_ARCHFILE_HPP_
