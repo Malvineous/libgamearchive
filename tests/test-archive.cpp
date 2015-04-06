1a9cc250abcc9c4fc6b53ec45d257ecfc8681818
Adam Nielsen <malvineous@shikadi.net>
2015-04-07 09:40:41 +1000

    tests: Set checkpoint for isinstance_other so exception shows offending handler

--- tests/test-archive.cpp
+++ tests/test-archive.cpp
@@ -462,10 +462,15 @@ void test_archive::test_isinstance_others()
 				this->skipInstDetect.begin(), this->skipInstDetect.end(), otherType
 			) != this->skipInstDetect.end()) continue;
 
-		BOOST_TEST_MESSAGE("Checking " << this->type
+		BOOST_CHECKPOINT("Checking " << this->type
 			<< " content against isInstance() for " << otherType);
 
-		BOOST_CHECK_MESSAGE(pTestType->isInstance(content) < ArchiveType::DefinitelyYes,
+		// Put this outside the BOOST_CHECK_MESSAGE macro so if an exception is
+		// thrown we can see the above BOOST_CHECKPOINT message telling us which
+		// handler is to blame.
+		auto isInstanceResult = pTestType->isInstance(content);
+
+		BOOST_CHECK_MESSAGE(isInstanceResult < ArchiveType::DefinitelyYes,
 			"isInstance() for " << otherType << " incorrectly recognises content for "
 			<< this->type);
 	}
