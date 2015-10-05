c139713c5616eca3425cd96dfe9a8bbab28faf8e
Adam Nielsen <malvineous@shikadi.net>
2015-10-05 12:05:32 +1000

    glb-raptor: #include <algorithm> for std::min

--- src/filter-glb-raptor.cpp
+++ src/filter-glb-raptor.cpp
@@ -18,6 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 #include <camoto/stream_filtered.hpp>
 #include <camoto/util.hpp> // std::make_unique
 #include "filter-glb-raptor.hpp"
