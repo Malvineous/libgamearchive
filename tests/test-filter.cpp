ad5396d96e241d50ae186d88b7e84d24cf13514b
Adam Nielsen <malvineous@shikadi.net>
2015-10-31 14:12:18 +1000

    Update filter test code to new class-based style
    This will make the tests neater and make it easier to implement more tests
    on the existing test data.

--- tests/test-filter.cpp
+++ tests/test-filter.cpp
@@ -1,6 +1,6 @@
 /**
  * @file   test-filter.cpp
- * @brief  Generic test code for filters.
+ * @brief  Generic test code for Filter class implementations.
  *
  * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
  *
@@ -18,118 +18,256 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <memory>
-#include <camoto/util.hpp> // std::make_unique
+#include <iomanip>
+#include <functional>
+#include <camoto/util.hpp>
 #include "test-filter.hpp"
 
+using namespace camoto;
+using namespace camoto::gamearchive;
+
 test_filter::test_filter()
-	:	in(std::make_unique<stream::string>())
+	:	init(false),
+		numInvalidContentTests(1)
 {
 }
 
-boost::test_tools::predicate_result test_filter::is_equal(const std::string& strExpected)
+void test_filter::addTests()
 {
-	auto in_filt = std::make_unique<stream::input_filtered>(
-		std::move(this->in),
-		std::move(this->filter)
-	);
-
-	stream::string out;
-	stream::copy(out, *in_filt);
-
-	// See if the stringstream now matches what we expected
-	return this->test_main::is_equal(strExpected, out.data);
-}
-
-void test_filter::test_equal_read(FilterType *ft, const std::string& strInput,
-	const std::string& strExpected)
-{
-	{
-	auto ss = std::make_unique<stream::string>(strInput);
-	auto filteredStream = ft->apply(
-		std::unique_ptr<stream::inout>(std::move(ss)),
-		stream::fn_notify_prefiltered_size()
-	);
-
-	stream::string out;
-	stream::copy(out, *filteredStream);
-	out.flush();
-
-	// See if the stringstream now matches what we expected
-	BOOST_CHECK_MESSAGE(this->test_main::is_equal(strExpected, out.data),
-		"Reading data through input/output filter failed");
-	}
-	{
-	auto ss = std::make_unique<stream::input_string>(strInput);
-	auto filteredStream = ft->apply(
-		std::unique_ptr<stream::input>(std::move(ss))
-	);
-
-	stream::string out;
-	stream::copy(out, *filteredStream);
-	out.flush();
-
-	// See if the stringstream now matches what we expected
-	BOOST_CHECK_MESSAGE(this->test_main::is_equal(strExpected, out.data),
-		"Reading data through input-only filter failed");
-	}
 	return;
 }
 
-void test_filter::test_equal_write(FilterType *ft, const std::string& strInput,
-	const std::string& strExpected)
+void test_filter::addBoundTest(std::function<void()> fnTest,
+	boost::unit_test::const_string file, std::size_t line,
+	boost::unit_test::const_string name)
 {
-	{
-	auto ss = std::make_unique<stream::string>();
-	const std::string *indata = &ss->data;
-	auto filteredStream = ft->apply(
-		std::unique_ptr<stream::inout>(std::move(ss)),
-		stream::fn_notify_prefiltered_size()
+	this->ts->add(
+		boost::unit_test::make_test_case(
+			std::bind(&test_filter::runTest, this, fnTest),
+			name, //createString(name << '[' << this->basename << ']'),
+			file, line
+		)
 	);
-
-	*filteredStream << strInput;
-	filteredStream->truncate_here();
-	filteredStream->flush();
-
-	// See if the stringstream now matches what we expected
-	BOOST_CHECK_MESSAGE(this->test_main::is_equal(strExpected, *indata),
-		"Writing data through input/output filter failed");
-	}
-	{
-	auto ss = std::make_unique<stream::output_string>();
-	const std::string *indata = &ss->data;
-	auto filteredStream = ft->apply(
-		std::unique_ptr<stream::output>(std::move(ss)),
-		stream::fn_notify_prefiltered_size()
-	);
-
-	*filteredStream << strInput;
-	filteredStream->truncate_here();
-	filteredStream->flush();
-
-	// See if the stringstream now matches what we expected
-	BOOST_CHECK_MESSAGE(this->test_main::is_equal(strExpected, *indata),
-		"Writing data through output-only filter failed");
-	}
 	return;
 }
 
-boost::test_tools::predicate_result test_filter::should_fail()
+void test_filter::runTest(std::function<void()> fnTest)
 {
-	try {
-		auto in_filt = std::make_shared<stream::input_filtered>(
-			std::move(this->in),
-			std::move(this->filter)
-		);
-		stream::string out;
-		stream::copy(out, *in_filt);
-	} catch (filter_error) {
-		// If we made it this far all is good
-		return boost::test_tools::predicate_result(true);
-	} catch (...) {
-		BOOST_TEST_CHECKPOINT("Got exception but wasn't filter_error");
-		return boost::test_tools::predicate_result(false);
-	}
-	BOOST_TEST_CHECKPOINT("filter_error exception was not raised");
-	return boost::test_tools::predicate_result(false);
+	this->prepareTest();
+	fnTest();
+	return;
+}
+
+void test_filter::prepareTest()
+{
+	// Make sure the test writer didn't forget to specify the filter ID
+	BOOST_REQUIRE(!this->type.empty());
+
+	this->pFilterType = FilterManager::byCode(this->type);
+	BOOST_REQUIRE_MESSAGE(this->pFilterType,
+		"Could not find filter type " << this->type);
+
+	return;
+}
+
+void test_filter::invalidContent(const std::string& content)
+{
+	auto fnTest = std::bind(&test_filter::test_invalidContent, this, content,
+		this->numInvalidContentTests);
+
+	this->addBoundTest(
+		std::bind(&test_filter::test_invalidContent, this, content,
+			this->numInvalidContentTests),
+		__FILE__, __LINE__,
+		createString("invalidcontent_i" << std::setfill('0') << std::setw(2)
+			<< this->numInvalidContentTests)
+	);
+/*
+	this->ts->add(
+		boost::unit_test::make_test_case(
+			std::bind(&test_filter::runTest, this, fnTest),
+			createString("test_filter[" << this->basename << "]::invalidcontent_i"
+				<< std::setfill('0') << std::setw(2) << this->numInvalidContentTests),
+			__FILE__, __LINE__
+		)
+	);
+*/
+	this->numInvalidContentTests++;
+	return;
+}
+
+void test_filter::test_invalidContent(const std::string& content,
+	unsigned int testNumber)
+{
+	BOOST_TEST_MESSAGE(this->basename << ": invalidContent_i"
+		<< std::setfill('0') << std::setw(2) << testNumber << ")");
+
+	auto pTestType = FilterManager::byCode(this->type);
+	BOOST_REQUIRE_MESSAGE(pTestType, "Could not find filter type " << this->type);
+
+	auto input = this->pFilterType->apply(
+		std::make_unique<stream::input_string>(content)
+	);
+
+	auto filterResult = std::make_unique<stream::string>();
+
+	// But that we get an error when trying to open the file
+	BOOST_CHECK_THROW(
+		stream::copy(*filterResult, *input);
+		,
+		stream::error
+	);
+
+	return;
+}
+
+void test_filter::content(const std::string& name, const std::string& filtered,
+	const std::string& plain, stream::len prefilteredSize)
+{
+	// Read through input filter
+	this->addBoundTest(
+		std::bind(&test_filter::test_content_read_in, this, filtered, plain),
+		__FILE__, __LINE__,
+		createString("content_read_in/" << name)
+	);
+
+	// Read through in/out filter
+	this->addBoundTest(
+		std::bind(&test_filter::test_content_read_inout, this, filtered, plain),
+		__FILE__, __LINE__,
+		createString("content_read_inout/" << name)
+	);
+
+	// Write through in/out filter
+	this->addBoundTest(
+		std::bind(&test_filter::test_content_write_out, this,
+			filtered, plain, prefilteredSize),
+		__FILE__, __LINE__,
+		createString("content_write_out/" << name)
+	);
+
+	// Write through in/out filter
+	this->addBoundTest(
+		std::bind(&test_filter::test_content_write_inout, this,
+			filtered, plain, prefilteredSize),
+		__FILE__, __LINE__,
+		createString("content_write_inout/" << name)
+	);
+
+	return;
+}
+
+void test_filter::test_content_read_in(const std::string& filtered,
+	const std::string& plain)
+{
+	BOOST_TEST_MESSAGE(this->basename << ": "
+		<< boost::unit_test::framework::current_test_case().p_name);
+	BOOST_REQUIRE(this->pFilterType); // Make sure prepareTest() was called
+
+	auto input = this->pFilterType->apply(
+		std::make_unique<stream::input_string>(filtered)
+	);
+
+	BOOST_TEST_CHECKPOINT("Read through input filter");
+	auto filterResult = std::make_unique<stream::string>();
+	stream::copy(*filterResult, *input);
+
+	BOOST_REQUIRE_MESSAGE(
+		this->is_equal(plain, filterResult->data),
+		"Reading through filter produced incorrect result"
+	);
+
+	return;
+}
+
+void test_filter::test_content_read_inout(const std::string& filtered,
+	const std::string& plain)
+{
+	BOOST_TEST_MESSAGE(this->basename << ": "
+		<< boost::unit_test::framework::current_test_case().p_name);
+	BOOST_REQUIRE(this->pFilterType); // Make sure prepareTest() was called
+
+	auto input = this->pFilterType->apply(
+		(std::unique_ptr<stream::inout>)std::make_unique<stream::string>(filtered),
+		nullptr // ensure we're calling the inout version
+	);
+
+	BOOST_TEST_CHECKPOINT("Read through in/out filter");
+	auto filterResult = std::make_unique<stream::string>();
+	stream::copy(*filterResult, *input);
+
+	BOOST_REQUIRE_MESSAGE(
+		this->is_equal(plain, filterResult->data),
+		"Reading through in/out filter produced incorrect result"
+	);
+
+	return;
+}
+
+void test_filter::test_content_write_out(const std::string& filtered,
+	const std::string& plain, stream::len prefilteredSize)
+{
+	BOOST_TEST_MESSAGE(this->basename << ": "
+		<< boost::unit_test::framework::current_test_case().p_name);
+	BOOST_REQUIRE(this->pFilterType); // Make sure prepareTest() was called
+
+	auto filterResult = std::make_unique<stream::output_string>();
+	auto& filterResult_data = filterResult->data;
+
+	stream::len setPrefiltered = 0;
+
+	auto output = this->pFilterType->apply(
+		(std::unique_ptr<stream::output>)std::move(filterResult),
+		[&setPrefiltered](stream::output_filtered* s, stream::len l) {
+			setPrefiltered = l;
+		}
+	);
+
+	BOOST_TEST_CHECKPOINT("Write through in/out filter");
+	output->write(plain);
+	output->flush();
+
+	// Make sure the prefiltered size set by the filter is what we are expecting
+	BOOST_CHECK_EQUAL(setPrefiltered, prefilteredSize);
+
+	BOOST_REQUIRE_MESSAGE(
+		this->is_equal(filtered, filterResult_data),
+		"Writing through in/out filter produced incorrect result"
+	);
+
+	return;
+}
+
+void test_filter::test_content_write_inout(const std::string& filtered,
+	const std::string& plain, stream::len prefilteredSize)
+{
+	BOOST_TEST_MESSAGE(this->basename << ": "
+		<< boost::unit_test::framework::current_test_case().p_name);
+	BOOST_REQUIRE(this->pFilterType); // Make sure prepareTest() was called
+
+	auto filterResult = std::make_unique<stream::string>();
+	auto& filterResult_data = filterResult->data;
+
+	stream::len setPrefiltered = 0;
+
+	auto output = this->pFilterType->apply(
+		(std::unique_ptr<stream::inout>)std::move(filterResult),
+		[&setPrefiltered](stream::output_filtered* s, stream::len l) {
+			setPrefiltered = l;
+		}
+	);
+
+	BOOST_TEST_CHECKPOINT("Write through in/out filter");
+	output->write(plain);
+	output->flush();
+
+	// Make sure the prefiltered size set by the filter is what we are expecting
+	BOOST_CHECK_EQUAL(setPrefiltered, prefilteredSize);
+
+	BOOST_REQUIRE_MESSAGE(
+		this->is_equal(filtered, filterResult_data),
+		"Writing through in/out filter produced incorrect result"
+	);
+
+	return;
 }
--- tests/test-filter.hpp
+++ tests/test-filter.hpp
@@ -1,6 +1,6 @@
 /**
  * @file   test-filter.hpp
- * @brief  Generic test code for filters.
+ * @brief  Generic test code for Filter class implementations.
  *
  * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
  *
@@ -18,10 +18,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <memory>
+#ifndef _CAMOTO_GAMEARCHIVE_TEST_FILTER_HPP_
+#define _CAMOTO_GAMEARCHIVE_TEST_FILTER_HPP_
+
+#include <map>
+#include <functional>
+#include <boost/test/unit_test.hpp>
 #include <camoto/stream_string.hpp>
-#include <camoto/stream_filtered.hpp>
-#include <camoto/gamearchive/filtertype.hpp>
+#include <camoto/gamearchive.hpp>
 #include "tests.hpp"
 
 // This header will only be used by test implementations.
@@ -31,15 +35,105 @@ using namespace camoto::gamearchive;
 class test_filter: public test_main
 {
 	public:
-		std::unique_ptr<stream::string> in;
-		std::unique_ptr<camoto::filter> filter;
-
+		/// Constructor sets some default values.
 		test_filter();
 
-		boost::test_tools::predicate_result is_equal(const std::string& strExpected);
-		void test_equal_read(camoto::gamearchive::FilterType *ft,
-			const std::string& strInput, const std::string& strExpected);
-		void test_equal_write(camoto::gamearchive::FilterType *ft,
-			const std::string& strInput, const std::string& strExpected);
-		boost::test_tools::predicate_result should_fail();
+		/// Add all the standard tests.
+		/**
+		 * This can be overridden by descendent classes to add more tests for
+		 * particular file formats.  If this is done, remember to call this
+		 * function from the overridden one or the standard tests won't get run.
+		 */
+		virtual void addTests();
+
+		/// Reset content back to a known state.
+		virtual void prepareTest();
+
+	protected:
+		/// Add a test to the suite.  Used by ADD_IMAGE_TEST().
+		void addBoundTest(std::function<void()> fnTest,
+			boost::unit_test::const_string file, std::size_t line,
+			boost::unit_test::const_string name);
+
+		/// Reset the image to the initial state and run the given test.
+		/**
+		 * @param empty
+		 *   true resets to an empty image (via ImageType::create()) while
+		 *   false resets to initialstate() and calls ImageType::open().
+		 *
+		 * @param fnTest
+		 *   Function to call once image is back to initial state.
+		 */
+		void runTest(std::function<void()> fnTest);
+
+		/// Add an invalidContent check to run later.
+		/**
+		 * These checks make sure files that are in the correct format
+		 * don't cause segfaults or infinite loops if the data is corrupted.
+		 *
+		 * @param content
+		 *   Content to pass as an archive to ImageType::isInstance() where
+		 *   it will be reported as a valid instance, then passed to
+		 *   ImageType::open(), where an exception should be thrown.
+		 */
+		void invalidContent(const std::string& content);
+
+		/// Add another read/write content filtering test.
+		/**
+		 * @param name
+		 *   Name to identify the test in error messages.
+		 *
+		 * @param filtered
+		 *   Filtered content (e.g. compressed, encrypted).
+		 *
+		 * @param plain
+		 *   Unfiltered content (e.g. uncompressed, plaintext).
+		 */
+		void content(const std::string& name, const std::string& filtered,
+			const std::string& plain, stream::len prefilteredSize);
+
+	protected:
+		/// Perform an invalidContent check now.
+		void test_invalidContent(const std::string& content,
+			unsigned int testNumber);
+
+		/// Perform a content check now, reading the data through a stream::input.
+		void test_content_read_in(const std::string& filtered,
+			const std::string& plain);
+
+		/// Perform a content check now, reading the data through a stream::inout.
+		void test_content_read_inout(const std::string& filtered,
+			const std::string& plain);
+
+		/// Perform a content check now, writing the data through a stream::output.
+		void test_content_write_out(const std::string& filtered,
+			const std::string& plain, stream::len prefilteredSize);
+
+		/// Perform a content check now, writing the data through a stream::inout.
+		void test_content_write_inout(const std::string& filtered,
+			const std::string& plain, stream::len prefilteredSize);
+
+		/// Factory class used to open images in this format.
+		FilterManager::handler_t pFilterType;
+
+	private:
+		/// Have we allocated pFilterType yet?
+		bool init;
+
+		/// Number of invalidData tests, used to number them sequentially.
+		unsigned int numInvalidContentTests;
+
+	public:
+		/// File type code for this format.
+		std::string type;
 };
+
+/// Add a test_image member function to the test suite
+#define ADD_FILTER_TEST(fn) \
+	this->test_filter::addBoundTest( \
+		std::bind(fn, this), \
+		__FILE__, __LINE__, \
+		BOOST_TEST_STRINGIZE(fn) \
+	);
+
+#endif // _CAMOTO_GAMEARCHIVE_TEST_FILTER_HPP_
